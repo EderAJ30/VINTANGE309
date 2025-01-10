f3fc8ed58ebbfae12f5c9f1cb10de382d162144f
EderAJ30 <EAJ30092004@gmail.com>
2025-01-10 01:19:39 -0600

    se subee config del index

--- index.js
+++ index.js
@@ -1,31 +1,75 @@
-import inicio from "./routes/inicio_router.js";
+// import = requequeeridos
 import express from "express";
-import db from "./config/db.js";
 
+import session from "express-session";
+
+// Registro Usuario
+
+import db from "./config/db.js";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+//import router_Verificar from "./routes/router_Verificar.js";
+//import { isAuthenticated } from "./middleware/middleware.js";
+
+// CONFIGURACIONES PAGINA
+
+// Crear la aplicacion 
 const app = express();
 
-//rendeerizar con pug o html
-app.set("view engine", "pug");
-app.set("views", "./views");
+// Procesar datos enviados desde forms
+app.use(express.urlencoded({ extended: true }));
 
-// carpeta publica para la vista 
-app.use(express.static("public"));
-
-//ruta por defecto y rutas del sistema
-app.use("/", inicio)
-
-//puerto dondee see abre el localhost
-const port = 2800;
-app.listen(port, () => {
-    console.log("Esperando peticiones en");
-});
-
-// Conectando a la base de datos
-try{
+// Conexión a la base de datos
+try {
     await db.authenticate();
     db.sync();
     console.log("Conexion exitosa a la base de datos");
-    
-}catch(error){
-    console.log(error); 
+} catch (error) {
+    console.log("Error de conexión a la base de datos:", error);
+    process.exit(1);  // Detiene el servidor si no se puede conectar a la base de datos
 }
+
+// Seteamos las variables de entorno
+dotenv.config({ path: ".env" });
+
+// Seteamos las cookies 
+app.use(cookieParser());
+
+app.set("view engine", "pug");
+app.set("views", "./views");
+app.use(express.static("public"));
+
+// Variables de Sesión
+/*
+app.use(session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true
+}));
+*/
+
+// Llamar a los routers
+// routing -> Ruta por default
+//app.use("/login", router_Login);
+//app.use("/register", router_Registro);
+//app.use("/logout", router_Logout);
+//app.use("/verificar", router_Verificar);
+
+// Middleware para verificar la sesión
+/*app.use(function(req, res, next) {
+    res.locals.loggedin = req.session.loggedin;
+    next();  // Usamos 'next()' para continuar el flujo
+}); */
+
+
+//app.use("/admin", router_crud);
+
+//rouuters quue usaremos
+app.use("/", inicio);
+app.use("/image", express.static("public/image"));
+
+// Definiendo el puerto -> Puerto de comunicación
+const port = 2800;
+app.listen(port, () => {
+    console.log(`Esperando peticiones en el puerto ${port}`);
+});
